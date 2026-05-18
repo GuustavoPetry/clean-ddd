ce45a3331fca5b7e2b93919a8d59a89a5c207c64
GuustavoPetry <ogustavopetry@gmail.com>
2026-05-18 14:47:13 -0300

    entities + answerQuestionService

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,19 @@
+import { Answer } from "../entities/answer";
+
+interface AnswerQuestionServiceRequest {
+    instructorId: string,
+    questionId: string,
+    content: string,
+}
+
+export class AnswerQuestionService {
+    execute({
+        instructorId, 
+        questionId,
+        content
+    }: AnswerQuestionServiceRequest) {
+        const answer = new Answer(content);
+
+        return answer;
+    }
+}
