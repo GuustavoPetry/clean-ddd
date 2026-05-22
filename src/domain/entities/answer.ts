8324e0b280126aca7278fc79993495da8b44bb8b
GuustavoPetry <ogustavopetry@gmail.com>
2026-05-22 06:30:34 -0300

    Getters & Setters das entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,6 +1,6 @@
-import { Entity } from "../../core/entities/entity";
-import { UniqueEntityID } from "../../core/entities/unique-entity-id";
-import { Optional } from "../../core/types/optional";
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
 
 interface AnswerProps {
     questionId: UniqueEntityID,
@@ -11,8 +11,36 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps> {
+    get questionId() {
+        return this.props.questionId;
+    }
+
+    get authorId() {
+        return this.props.authorId;
+    }
+
     get content() {
         return this.props.content;
+    }
+
+    get created_at() {
+        return this.props.created_at;
+    }
+
+    get updateAt() {
+        return this.props.updateAt;
+    }
+
+    get excerpt() {
+        return this.content.substring(0, 120).trimEnd().concat("...");
+    }
+
+    private touch() {
+        this.props.updateAt = new Date();
+    }
+
+    set content(content: string) {
+        this.props.content = content;
     }
 
     static create(
